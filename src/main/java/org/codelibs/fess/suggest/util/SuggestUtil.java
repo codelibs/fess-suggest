95918de9050796c7438ef7ccc79aaa422c2f4991
y_fujita <uu055650@CA0057.local>
2015-06-23 20:52:41 +0900

    add StandardQueryPaser

--- src/main/java/org/codelibs/fess/suggest/util/SuggestUtil.java
+++ src/main/java/org/codelibs/fess/suggest/util/SuggestUtil.java
@@ -1,32 +1,97 @@
 package org.codelibs.fess.suggest.util;
 
+import org.apache.lucene.queryparser.flexible.standard.StandardQueryParser;
+import org.apache.lucene.queryparser.flexible.standard.config.StandardQueryConfigHandler;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class SuggestUtil {
+    private static final int MAX_QUERY_TERM_NUM = 5;
+    private static final int MAX_QUERY_TERM_LENGTH = 48;
 
     private SuggestUtil() {
     }
 
-    public static String[] parseQuery(String query, String field) {
-        List<String> words = new ArrayList<>();
-        String[] elems = query.replace("(", " ").replace(")", " ").replaceAll(" +", " ").trim().split(" ");
-        for (String elem : elems) {
-            if (!elem.contains(":")) {
-                continue;
-            }
-            String[] pair = elem.split(":");
-            if (pair.length != 2 || !field.equals(pair[0])) {
-                continue;
-            }
-            if (!words.contains(pair[1])) {
-                words.add(pair[1]);
-            }
-        }
-        return words.toArray(new String[words.size()]);
-    }
-
     public static String createSuggestTextId(String text) {
         return String.valueOf(text.hashCode());
+    }
+
+    public static String[] parseQuery(final String q, final String field) {
+        final List<String> keywords = getKeywords(q, new String[] { field });
+        if (MAX_QUERY_TERM_NUM < keywords.size()) {
+            return null;
+        }
+        for (final String k : keywords) {
+            if (MAX_QUERY_TERM_LENGTH < k.length()) {
+                return null;
+            }
+        }
+        return keywords.toArray(new String[keywords.size()]);
+    }
+
+    public static List<String> getKeywords(final String q, final String[] fields) {
+        final List<String> keywords = new ArrayList<String>();
+        final List<TermQuery> termQueryList;
+        try {
+            StandardQueryParser parser = new StandardQueryParser();
+            parser.setDefaultOperator(StandardQueryConfigHandler.Operator.AND);
+
+            termQueryList = getTermQueryList(parser.parse(q, "default"), fields);
+        } catch (Exception e) {
+            return keywords;
+        }
+        for (final TermQuery tq : termQueryList) {
+            String text = tq.getTerm().text();
+            if (0 == text.length()) {
+                continue;
+            }
+            // duplicate
+            //String entry = field + QUERY_FIELD_SEPARATOR + text;
+            String entry = text;
+            if (keywords.contains(entry)) {
+                continue;
+            }
+            keywords.add(entry);
+        }
+        return keywords;
+    }
+
+    public static List<TermQuery> getTermQueryList(final Query query, final String[] fields) {
+        if (query instanceof BooleanQuery) {
+            final BooleanQuery booleanQuery = (BooleanQuery) query;
+            final BooleanClause[] clauses = booleanQuery.getClauses();
+            final List<TermQuery> queryList = new ArrayList<TermQuery>();
+            for (final BooleanClause clause : clauses) {
+                final Query q = clause.getQuery();
+                if (q instanceof BooleanQuery) {
+                    queryList.addAll(getTermQueryList(q, fields));
+                } else if (q instanceof TermQuery) {
+                    final TermQuery termQuery = (TermQuery) q;
+                    for (final String field : fields) {
+                        if (field.equals(termQuery.getTerm().field())) {
+                            queryList.add(termQuery);
+                        }
+                    }
+                }
+            }
+            return queryList;
+        } else if (query instanceof TermQuery) {
+            final TermQuery termQuery = (TermQuery) query;
+            for (final String field : fields) {
+                if (field.equals(termQuery.getTerm().field())) {
+                    final List<TermQuery> queryList = new ArrayList<TermQuery>(1);
+                    queryList.add(termQuery);
+                    return queryList;
+                }
+            }
+        }
+        return Collections.emptyList();
     }
 }
