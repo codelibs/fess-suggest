d69dc4ae20de8d7489a79e0befd8609e2f4ea60b
y_fujita <uu055650@CA0057.local>
2015-10-07 15:16:00 +0900

    add testcase

--- src/test/java/org/codelibs/fess/suggest/concurrent/DeferredTest.java
+++ src/test/java/org/codelibs/fess/suggest/concurrent/DeferredTest.java
@@ -46,6 +46,38 @@ public class DeferredTest {
     }
 
     @Test
+    public void test_doneBeforeReject() throws Exception {
+        final Deferred<SuggestResponse> deferred = new Deferred<>();
+
+        Thread th = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignore) {}
+            deferred.reject(new Exception());
+        });
+        th.start();
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        deferred.promise().error(error -> latch.countDown());
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void test_doneAfterReject() throws Exception {
+        final Deferred<SuggestResponse> deferred = new Deferred<>();
+
+        Thread th = new Thread(() -> {
+            deferred.reject(new Exception());
+        });
+        th.start();
+
+        Thread.sleep(1000);
+        final CountDownLatch latch = new CountDownLatch(1);
+        deferred.promise().error(error -> latch.countDown());
+        assertTrue(latch.await(10, TimeUnit.SECONDS));
+    }
+
+    @Test
     public void test_getResponseBeforeResolve() throws Exception {
         final Deferred<SuggestResponse> deferred = new Deferred<>();
 
