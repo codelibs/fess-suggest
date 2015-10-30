ee07074368efb89bfe79e534a77d77ecc5f26ccf
yfujita <yfujita@fujita-no-MacBook-Pro.local>
2015-10-30 17:00:10 +0900

    remove auto create index

--- src/main/java/org/codelibs/fess/suggest/SuggesterBuilder.java
+++ src/main/java/org/codelibs/fess/suggest/SuggesterBuilder.java
@@ -91,8 +91,6 @@ public class SuggesterBuilder {
             threadPool = Executors.newFixedThreadPool(threadPoolSize);
         }
 
-        final Suggester suggester = new Suggester(client, settings, readingConverter, normalizer, analyzer, threadPool);
-        suggester.createIndexIfNothing();
-        return suggester;
+        return new Suggester(client, settings, readingConverter, normalizer, analyzer, threadPool);
     }
 }
