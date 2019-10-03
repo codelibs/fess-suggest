fe75cf2647c45f8239dd885a637dfc1375040fd2
Shinsuke Sugaya <shinsuke@apache.org>
2019-10-03 11:02:21 +0900

    replace with single-node

--- src/test/java/org/codelibs/fess/suggest/settings/BadWordSettingsTest.java
+++ src/test/java/org/codelibs/fess/suggest/settings/BadWordSettingsTest.java
@@ -40,8 +40,9 @@ public class BadWordSettingsTest {
         runner = new ElasticsearchClusterRunner();
         runner.onBuild((number, settingsBuilder) -> {
             settingsBuilder.put("http.cors.enabled", true);
-            settingsBuilder.putList("discovery.seed_hosts", "127.0.0.1:9301");
-            settingsBuilder.putList("cluster.initial_master_nodes", "127.0.0.1:9301");
+            settingsBuilder.put("discovery.type", "single-node");
+            // settingsBuilder.putList("discovery.seed_hosts", "127.0.0.1:9301");
+            // settingsBuilder.putList("cluster.initial_master_nodes", "127.0.0.1:9301");
         }).build(newConfigs().clusterName("ArraySettingsTest").numOfNode(1)
                 .pluginTypes("org.codelibs.elasticsearch.kuromoji.ipadic.neologd.KuromojiNeologdPlugin"));
         runner.ensureYellow();
